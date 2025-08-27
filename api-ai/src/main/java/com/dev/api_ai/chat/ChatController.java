ef1b95fe670cee4ae743052a5593a3158f113523
Henrique da Silva <henriquec.dasilva@hotmail.com>
2025-08-27 20:44:50 -0300

    SimpleChat

---
+++ api-ai/src/main/java/com/dev/api_ai/chat/ChatController.java
@@ -0,0 +1,28 @@
+package com.dev.api_ai.chat;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/chat")
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @PostMapping()
+    public ChatMessage simpleChat(@RequestBody ChatMessage message) {
+        var response = this.chatClient.prompt()
+                .user(message.message())
+                .call()
+                .content();
+
+        return new ChatMessage(response);
+    }
+}
